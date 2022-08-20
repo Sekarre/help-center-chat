e56b44266c3d6df40cad56f66b5c1c03c1ac8b30
Sekarre <zimny2121@gmail.com>
2022-08-20 20:18:36 +0200

    initial commit

---
+++ src/test/java/com/sekarre/helpcenterchat/HelpCenterChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekarre.helpcenterchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpCenterChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
