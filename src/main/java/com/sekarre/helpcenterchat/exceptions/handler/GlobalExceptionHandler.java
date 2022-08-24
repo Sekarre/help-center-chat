c776eeb4394449a570612638ce22b148833b8b63
Sekarre <zimny2121@gmail.com>
2022-08-24 16:41:11 +0200

    Fixed error handling status

--- src/main/java/com/sekarre/helpcenterchat/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sekarre/helpcenterchat/exceptions/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sekarre.helpcenterchat.exceptions.handler;
 import com.sekarre.helpcenterchat.exceptions.AppRuntimeException;
 import com.sekarre.helpcenterchat.util.DateUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,27 +15,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = AppRuntimeException.class)
-    public ResponseEntity<ErrorMessage> handleChatNotFoundException(AppRuntimeException e) {
+    public ResponseEntity<ErrorMessage> handleAppRuntimeException(AppRuntimeException e) {
         log.error(e.getMessage());
-        return ResponseEntity.ok(getCustomErrorMessage(e.getMessage()));
+        return new ResponseEntity<>(getCustomErrorMessage(e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = UsernameNotFoundException.class)
     public ResponseEntity<ErrorMessage> handleAuthenticationException(UsernameNotFoundException e) {
         log.error(e.getMessage());
-        return ResponseEntity.ok(getCustomErrorMessage(e.getMessage()));
+        return new ResponseEntity<>(getCustomErrorMessage(e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(value = AuthenticationException.class)
     public ResponseEntity<ErrorMessage> handleAuthenticationException(AuthenticationException e) {
         log.error(e.getMessage());
-        return ResponseEntity.ok(getCustomErrorMessage(e.getMessage()));
+        return new ResponseEntity<>(getCustomErrorMessage(e.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<ErrorMessage> handleIllegalArgumentException(IllegalArgumentException e) {
         log.error(e.getMessage());
-        return ResponseEntity.ok(getCustomErrorMessage(e.getMessage()));
+        return new ResponseEntity<>(getCustomErrorMessage(e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     private ErrorMessage getCustomErrorMessage(String e) {
